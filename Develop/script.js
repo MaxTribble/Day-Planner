b3b6c89563d89248e4872a93f23427a508a3d857
Max Tribble <maxtribble@live.com>
2022-12-05 12:15:35 -0700

    added a local save to what is typed in the text area and it calls the local save back when the page is refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,7 @@ var fifteen = document.getElementById("hour-15")
 var sixteen = document.getElementById("hour-16")
 var seventeen = document.getElementById("hour-17")
 var all = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen]
+var saveBtn = document.getElementsByClassName("saveBtn")
 
 currentDay.innerHTML=current
 
@@ -37,31 +38,15 @@ for(i=0; i<9;i++){
   all[i].classList.add("past")
   all[i].classList.remove("present")
 }}
-//document.querySelector
-function save(){
-  console.log(this.parentNode.querySelector("textarea"))
+
+function saveSchedule(){
+  localStorage.setItem(this.parentNode.id, this.parentNode.querySelector("textarea").value)
 }
-var sibling = document.querySelector("texturearea")
-var Btn = document.getElementsByClassName("saveBtn")
-console.log(Btn[0])
-Btn[0].addEventListener("click", save);
-//$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-//});
+
+for(i=0; i<all.length; i++){
+saveBtn[i].addEventListener("click", saveSchedule);
+}
+
+for(i=0; i<all.length; i++){
+  all[i].querySelector("textarea").value=localStorage.getItem(all[i].id)
+}
